31132dde73c969d7b20d11dfa3a1657df0732319
Pietro <pietroalexandripicoli@gmail.com>
2026-06-23 16:36:35 -0300

    Removido target de rotas (Mantido Guia com target). Adicionado css em mudanca de endereco com um css proprio. Adicionado button voltar em mudanca de endereco. Feito padrao de Coleta de dados (Utilizarei nas outras opcoes, ja que é um padrão). Subido para Web Service em Render, OK!.

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = 3000;
+
+// Porta dinâmica para funcionar no Render (e localmente também)
+const PORT = process.env.PORT || 3000;
 
 // Serve para arquivos estáticos (css, js, images) da pasta public
 app.use(express.static(path.join(__dirname, "public")));
@@ -11,17 +13,17 @@ app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (_, res) => res.sendFile(path.join(__dirname, "views", "index.html")));
 
 // Rota - Sem Conexão 
-app.get("/sem-conexao", (_, res) => res.sendFile(path.join(__dirname, "views", "/options-html/sem-conexao.html")));
+app.get("/sem-conexao", (_, res) => res.sendFile(path.join(__dirname, "views", "options-html", "sem-conexao.html")));
 
 // Rota - Conexão Lenta
-app.get("/conexao-lenta", (_, res) => res.sendFile(path.join(__dirname, "views", "/options-html/conexao-lenta.html")));
+app.get("/conexao-lenta", (_, res) => res.sendFile(path.join(__dirname, "views", "options-html", "conexao-lenta.html")));
 
 // Rota - Mudança de Endereço
-app.get("/mudanca-endereco", (_, res) => res.sendFile(path.join(__dirname, "views", "/options-html/mudanca-endereco.html")));
+app.get("/mudanca-endereco", (_, res) => res.sendFile(path.join(__dirname, "views", "options-html", "mudanca-endereco.html")));
 
 // Rota - Suporte de SVA's
-app.get("/sva", (_, res) => res.sendFile(path.join(__dirname, "views", "/options-html/sva.html")));
+app.get("/sva", (_, res) => res.sendFile(path.join(__dirname, "views", "options-html", "sva.html")));
 
 // ---------------------------------------- //
 // Inicia o servidor na porta especificada
-app.listen(port, () => console.log(`Rodando em http://localhost:${port}`));
+app.listen(PORT, () => console.log(`Rodando em http://localhost:${PORT}`));
