c502a44489677072258a8deca17aad2767035012
Pietro <162758840+PietroAlexandria@users.noreply.github.com>
2026-06-27 18:19:56 +0000

    chore: adicionado ESLint feat: functionAddContact funcionando corretamente para adicionar mais de um contato ao agendamento, implementado até agora apenas no sem-conexao. wip: Prosseguindo com o layout de sem-conexao.

---
+++ public/js/sharedFunctions/functionAddContact.js
@@ -0,0 +1,22 @@
+let contadorContatos = 1;
+
+function adicionarContato() {
+    contadorContatos++;
+    const listaContatos = document.getElementById('lista-contatos');
+
+    const div = document.createElement('div');
+    div.classList.add('campo-contato');
+    div.id = 'contato-' + contadorContatos;
+
+    div.innerHTML = `
+        <input type="text" name="contato" maxlength="15" placeholder="(XX) XXXXX-XXXX" oninput="mascaraContato(this)">
+        <button class="btn-remove" onclick="removerContato(${contadorContatos})">X</button>
+    `;
+
+    listaContatos.appendChild(div);
+}
+
+function removerContato(id) {
+    const el = document.getElementById('contato-' + id);
+    if (el) {el.remove();}
+}
